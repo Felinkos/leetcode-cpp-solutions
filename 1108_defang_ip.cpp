cd2b73e8d82d1aa330ae28f67cb8b391f1802959
Felinkos <full.felinkosi@gmail.com>
2026-08-30 22:36:06 +0400

    feat: solve 1108. Defanging an IP Adress

---
+++ 1108_defang_ip.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+class Solution {
+public:
+    std::string defangIPaddr(std::string address) {
+        std::string result = "";
+        for (int i = 0; i < address.size(); ++i) {
+            if (address[i] == '.') {
+                result += "[.]";
+            } else {
+                result += address[i];
+            }
+        }
+        return result;
+    }
+};
