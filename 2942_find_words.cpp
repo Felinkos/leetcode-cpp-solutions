90fd63535145e04955616649d0055bc2146d6dbe
Felinkos <full.felinkosi@gmail.com>
2026-09-02 21:17:02 +0400

    feat: solve 2942. Find Words Containing Character using std::string::find

---
+++ 2942_find_words.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    std::vector<int> findWordsContaining(std::vector<std::string>& words, char x) {
+        std::vector<int> ans;
+        for (int i = 0; i < words.size(); ++i) {
+            if (words[i].find(x) != std::string::npos) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
