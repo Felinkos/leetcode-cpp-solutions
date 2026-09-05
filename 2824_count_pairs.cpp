5b46f5602f6bcdaf2e9e23cea193339b55ff2afa
Felinkos <full.felinkosi@gmail.com>
2026-09-05 19:03:08 +0400

    feat: solve 2824. Count Pairs Whose Sum is Less than Target using nested loops

---
+++ 2824_count_pairs.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+class Solution {
+public:
+    int countPairs(std::vector<int>& nums, int target) {
+        int count = 0;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (nums[i] + nums[j] < target) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+};
