e215ff76f2849288c891b65288903c97306e4751
Felinkos <full.felinkosi@gmail.com>
2026-09-09 20:17:01 +0400

    feat: solve 1365. How Many Numbers Are Smaller Than the Current Number using full scan

---
+++ 1365_smaller_numbers.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> smallerNumbersThanCurrent(std::vector<int>& nums) {
+        std::vector<int> ans;
+        for (int i = 0; i < nums.size(); ++i) {
+            int count = 0;
+            for (int j = 0; j < nums.size(); ++j) {
+                if (nums[i] > nums[j]) {
+                    ++count;
+                }
+            }
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
